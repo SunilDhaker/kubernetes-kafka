0f03300859e78e1af67b8f24229417a7e0b89c20
sunil@fretron.com <sunil@fretron.com>
2017-11-09 19:29:43 +0530

    +semver:major

---
+++ gcb/index.js
@@ -0,0 +1,48 @@
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
+const SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+
+const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+
+// subscribe is the main function called by Cloud Functions.
+module.exports.subscribe = (event, callback) => {
+ 
+ console.log(event)
+  const build = eventToBuild(event.data.data);
+
+// Skip if the current status is not in the status list.
+// Add additional statues to list if you'd like:
+// QUEUED, WORKING, SUCCESS, FAILURE,
+// INTERNAL_ERROR, TIMEOUT, CANCELLED
+  const status = ['SUCCESS', 'FAILURE', 'INTERNAL_ERROR', 'TIMEOUT'];
+  if (status.indexOf(build.status) === -1) {
+    return callback();
+  }
+
+  // Send message to Slack.
+  const message = createSlackMessage(build);
+  webhook.send(message, callback);
+};
+
+// eventToBuild transforms pubsub event message to a build object.
+const eventToBuild = (data) => {
+  return JSON.parse(new Buffer(data, 'base64').toString());
+}
+
+// createSlackMessage create a message from a build object.
+const createSlackMessage = (build) => {
+  let message = {
+   text: JSON.stringify(build),
+    mrkdwn: true,
+    attachments: [
+      {
+        title: 'Build logs',
+        title_link: build.logUrl,
+        fields: [{
+          title: 'Status',
+          value: build.status
+        }]
+      }
+    ]
+  };
+  return message
+}
